c756e7ff8e663dbdff9314fade1e1c42f6e2e11d
Harry_the_Pott <Johanneswirth10@gmail.com>
2023-05-30 14:33:59 +0200

    Changed some keys in Keymap

--- keyboards/handwired/tractyl_manuform/5x6_right/keymaps/johannes/keymap.c
+++ keyboards/handwired/tractyl_manuform/5x6_right/keymaps/johannes/keymap.c
@@ -111,7 +111,7 @@ const key_override_t** key_overrides     = (const key_override_t*[]){&lcbr_key_o
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_DVORAK] = LAYOUT_5x6_right(
                          KC_ESC,     KC_1,    KC_2,     KC_3 ,   KC_4,    KC_5,                  KC_6,    KC_7,       KC_8,      KC_9,      KC_0,    KC_EQL,
-                         KC_TAB,     KC_Q,    KC_W,     KC_E,    KC_R,    KC_T,                  KC_Z,    KC_U,       KC_I,      KC_O,      KC_P,    KC_MINS,
+                         KC_LSFT,     KC_Q,    KC_W,     KC_E,    KC_R,    KC_T,                  KC_Z,    KC_U,       KC_I,      KC_O,      KC_P,    KC_MINS,
                          KC_TAB,     KC_A,    KC_S,     MEH_T(KC_D),    C_S_T(KC_F),    KC_G,                  KC_H,    KC_J,       KC_K,      KC_L,      KC_SCLN, KC_QUOT,
                          TG(RAISE),      KC_Y,    KC_X,     KC_C,    KC_V,    KC_B,                  KC_N,    KC_M,       KC_COMM,   KC_DOT,    KC_SLSH, KC_BSLS,
                                            KC_LBRC,  KC_RBRC,                                                      KC_PGUP,     LOWER,
